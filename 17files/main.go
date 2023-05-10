8d408e9cb3433499da7f5009c01498494a29e1fe
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2023-05-10 14:26:14 +0530

    read file

--- 17files/main.go
+++ 17files/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -27,5 +28,18 @@ func main()  {
 
 	 defer file.Close()
 
+	 readFile("./myFile.text")
+}
 
+func readFile(filename string){
+	databyte,err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+	
+	fmt.Println("Raw databytes ",string(databyte))
+}
+
+func checkNilErr(err error){
+	if err != nil{
+		panic(err)
+	}
 }
