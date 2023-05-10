021beb4c5858943bf451514d55e738aeba354faa
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2023-05-10 11:16:20 +0530

    structs

---
+++ 10structs/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Structs in golang")
+	//no inheretence, no super or parent
+	pratim := User {"pratim","pratim@go.dev",true,18}
+	fmt.Println(pratim)
+
+	fmt.Printf("The value of %+v ",pratim)
+
+	fmt.Printf("The email is %v ",pratim.Email)
+}
+
+type User struct {
+	Name string
+	Email string
+	Status bool
+	Age     int
+}
